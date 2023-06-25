ff52aed3b3c45688f1788f8964cfafa45b2e7cad
Rick Buitrago <rbuitragoc@gmail.com>
2023-06-25 17:52:19 +0200

    moving all files to desired folder structures

--- src/test_rembg.py
+++ src/test_rembg.py
@@ -1,7 +1,7 @@
 from rembg import remove
 from PIL import Image
-input_path = 'whatever.jpeg'
-output_path = 'whatever.png'
+input_path = 'resource/whatever.jpeg'
+output_path = 'output/whatever.png'
 inp = Image.open(input_path)
 output = remove(inp)
 output.save(output_path)
